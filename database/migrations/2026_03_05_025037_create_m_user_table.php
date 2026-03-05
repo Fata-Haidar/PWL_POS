b1c6f1722e1935b68e831bffe3bee5e292e5df23
Fata-Haidar <fatahaidaraly@gmail.com>
2026-03-05 09:56:40 +0700

    rubah

--- database/migrations/2026_03_05_025037_create_m_user_table.php
+++ database/migrations/2026_03_05_025037_create_m_user_table.php
@@ -17,10 +17,8 @@ return new class extends Migration
             $table->string('username',20)->unique();
             $table->string('nama',100);
             $table->string('password');
-            $table->timestamps();
-            $table->foreign('level_id')
-            ->references('level_id')
-            ->on('m_level');
+            $table->timestamps();w
+            $table->foreign('level_id')->references('level_id')->on('m_level');
         });
     }
 
