d81b9c178fa92861eb85261c33dd0890c6892e7a
Fata-Haidar <fatahaidaraly@gmail.com>
2026-03-05 10:33:53 +0700

    Praktikum-2.2

--- database/migrations/2026_03_05_025037_create_m_user_table.php
+++ database/migrations/2026_03_05_025037_create_m_user_table.php
@@ -11,13 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('m_user', function (Blueprint $table) {
+        Schema::Create('m_user', function (Blueprint $table) {
             $table->id('user_id');
             $table->unsignedBigInteger('level_id')->index();
             $table->string('username',20)->unique();
             $table->string('nama',100);
             $table->string('password');
-            $table->timestamps();w
+            $table->timestamps();
             $table->foreign('level_id')->references('level_id')->on('m_level');
         });
     }
